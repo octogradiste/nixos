bc11ebb94477f7af1ed5e12824eca8b26a81afe1
octogradiste <colin.berger@outlook.com>
2025-02-19 20:51:15 +0100

    feat: Basic Hyprland setup

--- configuration.nix
+++ configuration.nix
@@ -41,12 +41,14 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
+  programs.hyprland.enable = true;
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
--- home.nix
+++ home.nix
@@ -7,7 +7,96 @@
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     htop
+    xournalpp
+    nodejs_23
   ];
+
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = [ "~/nixos/wallpaper.jpg" ];
+      wallpaper = [ ",~/nixos/wallpaper.jpg" ];
+    };
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, B, exec, firefox"
+        "$mod, T, exec, alacritty"
+	"$mod, SPACE, exec, wofi --show drun"
+
+	"$mod, E, killactive"
+	"$mod, F, fullscreen, 0"
+
+	"$mod, L, movefocus, r"
+	"$mod, H, movefocus, l"
+	"$mod, K, movefocus, u"
+	"$mod, J, movefocus, d"
+
+      ];
+      monitor = [
+        "eDP-1, 1920x1080, 0x0, 1"
+	",preferred, auto-left, 1.5"
+      ];
+      input = {
+        kb_layout = "ch";
+        kb_variant = "fr";
+      };
+      misc = {
+        disable_hyprland_logo = true;
+	disable_splash_rendering = true;
+      };
+      exec-once = "waybar";
+    };
+    extraConfig = ''
+bind=$mod,R,submap,resize
+
+submap=resize
+
+$step=20
+
+binde=,L,resizeactive,$step 0
+binde=,H,resizeactive,-$step 0
+binde=,K,resizeactive,0 -$step
+binde=,J,resizeactive,0 $step
+
+bind=SHIFT,L,movewindow,r
+bind=SHIFT,H,movewindow,l
+bind=SHIFT,K,movewindow,u
+bind=SHIFT,J,movewindow,d
+
+bind=,ESCAPE,submap,reset
+bind=,ENTER,submap,reset
+
+submap=reset
+    '';
+  };
+
+  programs.wofi = {
+    enable = true;
+    settings = {
+      gtk_dark = true;
+    };
+    style = "*{ font-family: monospace; }";
+  };
+
+  programs.waybar = {
+    enable = true;
+    settings = [{
+      layer = "top";
+      modules-left = [
+        "hyprland/workspaces"
+        "hyprland/submap"
+      ];
+      modules-right = [
+        "battery"
+        "clock"
+      ];
+    }];
+  };
 
   programs.firefox.enable = true;
 
