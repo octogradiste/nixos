95bd188cb14719c323265f53494bf34f18fdbc3e
octogradiste <colin.berger@outlook.com>
2025-03-02 13:16:09 +0100

    feat: Use JetbrainsMono Nerd Font as default monospace font

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -4,5 +4,11 @@
     image = ../wallpaper.jpg;
     cursor.size = 24;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+    };
   };
 }
