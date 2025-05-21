de49e79c42efe0ebfc3ccb5369f00b42ef9bc3a1
octogradiste <colin.berger@outlook.com>
2025-05-21 22:04:54 +0200

    Enable git lfs

--- home/programs/terminal/git.nix
+++ home/programs/terminal/git.nix
@@ -1,6 +1,8 @@
-{...}: {
+{ ... }:
+{
   programs.git = {
     enable = true;
+    lfs.enable = true;
     userName = "octogradiste";
     userEmail = "colin.berger@outlook.com";
     extraConfig = {
