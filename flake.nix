a5ddbc16f776b6b4e0e8950def9edfe31ea5ba40
octogradiste <colin.berger@outlook.com>
2025-06-17 18:47:16 +0200

    Upgrade to unstable

--- flake.nix
+++ flake.nix
@@ -2,51 +2,51 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nvf = {
-      url = "github:notashelf/nvf";
+    stylix = {
+      url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:danth/stylix/release-24.11";
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ {
-    nixpkgs,
-    home-manager,
-    nvf,
-    stylix,
-    ...
-  }: let
-    system = "x86_64-linux";
-    username = "colin";
-  in {
-    nixosConfigurations = {
-      yoga = nixpkgs.lib.nixosSystem {
-        system = system;
-        modules = [
-          ./nixos/configuration.nix
-          ./modules
-          nvf.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.colin = import ./home/home.nix;
-            home-manager.sharedModules = [stylix.homeManagerModules.stylix];
-            home-manager.extraSpecialArgs = {
-              inherit inputs system username;
-            };
-          }
-        ];
+  outputs =
+    inputs@{
+      nixpkgs,
+      home-manager,
+      stylix,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      username = "colin";
+    in
+    {
+      nixosConfigurations = {
+        yoga = nixpkgs.lib.nixosSystem {
+          system = system;
+          modules = [
+            ./nixos/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.colin = import ./home/home.nix;
+              home-manager.sharedModules = [ stylix.homeModules.stylix ];
+              home-manager.extraSpecialArgs = {
+                inherit inputs system username;
+              };
+              home-manager.backupFileExtension = "bak";
+            }
+          ];
+        };
       };
     };
-  };
 }
