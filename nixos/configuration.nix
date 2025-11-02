7cc033c5adaa274000042462f77cfbf575d4e301
octogradiste <colin.berger@outlook.com>
2025-11-02 18:45:09 +0100

    Add devenv cache source

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,6 +19,12 @@
       "nix-command"
       "flakes"
     ];
+    extra-substituters = [
+      "https://devenv.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
   };
 
   networking.hostName = "yoga"; # Define your hostname.
